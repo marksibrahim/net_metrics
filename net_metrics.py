efa6eba2d3b25b6344423d9ae55b2737722deaa1
Mark I <mark.ibhm@gmail.com>
2015-11-30 16:11:39 -0500

    created network class

--- net_metrics.py
+++ net_metrics.py
@@ -1,22 +1,69 @@
 from collections import defaultdict
 
-def compute_traversal_visits(net):
+class Network():
     """
-    returns a dictionary: nodes --> number of visits
-    input net is a dictionary of nodes and  single outward link
+    a directed network object with methods
+    to characterize the structure
+
+    input is a dictionary of nodes and single outward link
+    node --> link
     """
-    #initializes dictionary with 0 as default value 
-    traversal_visits = defaultdict(int)
 
-    for node in net:
-        link = net.get(node, False)
-        traversed = []
-        while link not in traversed and link:
-            traversal_visits[link] += 1
-            traversed.append(link)
-            link = net.get(link, False)
-    return traversal_visits
+    def __init__(self, net_dict):
+        self.net = net_dict
+        self.traversal_visits = self.compute_traversal_visits(self.net)
+        self.cycles = self.identify_cycles(self.net)
+        self.traversal_funnels = self.compute_traversal_funnels(self.net)
 
-        
+    def compute_traversal_visits(self, net):
+        """
+        returns a dictionary: nodes --> number of visits
+        """
+        #initializes dictionary with 0 as default value 
+        traversal_visits = defaultdict(int)
+
+        for node in net:
+            link = net.get(node, False)
+            traversed = []
+            while link not in traversed and link:
+                traversal_visits[link] += 1
+                traversed.append(link)
+                link = net.get(link, False)
+        return traversal_visits
+
+    def identify_cycles(self, net):
+        """
+        returns a list of cycles 
+        cycles are stored as sets: {'A', 'B', 'C'} 
+        """
+        cycles = []
+
+        for node in net:
+            link = net.get(node, False)
+            traversed = []
+            while link not in traversed and link:
+                traversed.append(link)
+                link = net.get(link, False)
+            #capture cycle
+            if link in traversed: 
+                cycle = traversed[traversed.index(link):]
+                #add cycle if not already found
+                if set(cycle) not in cycles:
+                    cycles.append(set(cycle))
+        return cycles
+            
+
+    def compute_traversal_funnels(self, net):
+        """
+        returns a dictionary: nodes --> number of visits
+        input net is a dictionary of nodes and single outward link
+        """
+        #initializes dictionary with 0 as default value 
+        traversal_funnels = defaultdict(int)
+
+        for node in net:
+            pass
+        return traversal_funnels
+
         
 
--- tests.py
+++ tests.py
@@ -1,24 +1,35 @@
 import net_metrics
 
 #same as sample network described in Wikipedia paper
-sample_net = {'A': 'B', 'B':'C', 'C':'A', 'D':'C', 'E':'C',
+sample_net_dict = {'A': 'B', 'B':'C', 'C':'A', 'D':'C', 'E':'C',
               'G':'E', 'F':'E'}
 
-#sample net with list values (one node pointing to several nodes)
-sample_net_multiple_nodes = {'A':['B'], 'B':['C'], 'C':['A'], 'E':['C'], 'D':['E', 'C']}
+#create instance of network object from net_metrics
+sample_net = net_metrics.Network(sample_net_dict)
 
 def test_traversal_visits():
     """
     tests result of traversal visits computation
     """
-    traversal_visits = net_metrics.compute_traversal_visits(sample_net)
+    traversal_visits = sample_net.traversal_visits
     assert traversal_visits['A'] == 7 
     assert traversal_visits['E'] == 2 
     assert traversal_visits['G'] == 0
 
+def test_cycles():
+    """
+    tests correct identification of cycles
+    """
+    cycles = sample_net.cycles
+    assert set(['A', 'B', 'C']) in cycles
+    assert set(['A', 'B']) not in cycles
+    assert set(['A', 'B', 'C', 'D']) not in cycles
     
 def test_traversal_funnels():
     """
     tests result of traversal funnels computation
     """
-    pass
+    traversal_funnels = sample_net.traversal_funnels
+    assert traversal_funnels['A'] == 0
+    assert traversal_funnels['B'] == 0
+    #assert traversal_funnels['C'] == 4
